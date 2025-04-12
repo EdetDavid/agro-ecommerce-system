796a4b80dce26037a6cde43489a33bc63699fb67
Dvooskid <dvooskid1234@gmail.com>
2025-04-12 18:47:15 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://https://agro-ecommerce-system-backend.onrender.com/api";
+  process.env.REACT_APP_API_URL || "https://agro-ecommerce-system-backend.onrender.com/api";
 
 // --- Create dedicated Axios instance ---
 const apiClient = axios.create({
